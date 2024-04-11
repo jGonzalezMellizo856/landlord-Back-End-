090adffc99a07ea786d6d6723d4ce4a3ebcdc27c
jGonzalezMellizo856 <149606249+jGonzalezMellizo856@users.noreply.github.com>
2024-04-11 01:57:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const PORT  = 10000;
 
-
+app.use(cors());
 app.use(express.json());
+
+app.get('/products/:id', function (req, res, next){
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+
+});
 
 app.post('/', (req, res) => {
     console.log(req.body);
@@ -12,4 +18,5 @@ app.post('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
+    console.log(`ors-enabled web server listening on ${PORT}`);
 });
