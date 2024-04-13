214b513b0651b07a0ec142aac63060f1d5580d53
jGonzalezMellizo856 <149606249+jGonzalezMellizo856@users.noreply.github.com>
2024-04-12 23:36:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
 const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
 const app = express();
-const PORT = 3000;
+var cors = require('cors');
+const PORT  = 10000;
 
-// Enable CORS middleware
 app.use(cors());
 app.use(express.json());
 
-// Define a route to proxy requests to the Render API
-app.post('/upload', async (req, res) => {
-    try {
-        const url = 'https://api.render.com/deploy/srv-cobpig5jm4es739ta7s0?key=bn7v8jQ4_7Y';
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(req.body)
-        });
-        
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
+app.get('/products/:id', function (req, res, next){
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+
 });
 
-// Start the server
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json({message: 'data recieved on server'});
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on ${PORT}`);
+    console.log(`ors-enabled web server listening on ${PORT}`);
 });
