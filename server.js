e633fd040eb7112e9689e247d1451721bad7173f
jGonzalezMellizo856 <149606249+jGonzalezMellizo856@users.noreply.github.com>
2024-04-12 23:11:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch'); // Import 'node-fetch' for making HTTP requests
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch'; // Import 'node-fetch' for making HTTP requests
 const app = express();
 const PORT = 3000;
 
